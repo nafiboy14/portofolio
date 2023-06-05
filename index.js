c6a13d9e0a1214eee2b0782fd241eebd9929e0f2
nafiboy14 <nafiboy14@gmail.com>
2023-06-05 16:39:40 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// Aos Animation//
+AOS.init({
+   // Global settings:
+   disable: false,
+   startEvent: 'DOMContentLoaded',
+   initClassName: 'aos-init', 
+   animatedClassName: 'aos-animate', 
+   useClassNames: false, 
+   disableMutationObserver: false, 
+   debounceDelay: 50, 
+   throttleDelay: 99, 
+   
+
+   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+   offset: 120, 
+   delay: 0, 
+   duration: 1500, 
+   easing: 'ease', 
+   once: false, 
+   mirror: false, 
+   anchorPlacement: 'top-bottom',
+
+});
+// Aos Animation//
